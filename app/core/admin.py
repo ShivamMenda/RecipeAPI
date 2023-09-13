48e14c7565a177384bd0f4c846e4cb6faeec8c39
Shivam Menda <shivammenda2002@gmail.com>
2023-09-13 18:43:10 +0530

    Configured docs and admin portal.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin # noqa
+"""
+Django admin custom
+"""
 
-# Register your models here.
+from django.contrib import admin # noqa
+from django.contrib.auth.admin import UserAdmin
+
+from core import models
+
+class UserAdminManager(UserAdmin):
+    """Define the admin pages for users"""
+    ordering=['id']
+    list_display=['email','name']
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email','password'
+            ),
+        }),
+        (('Permissions'), {
+            "fields": (
+                'is_active','is_staff','is_superuser'
+            ),
+        }),
+         (('Important dates'), {
+            "fields": (
+                'last_login',
+            ),
+        }),
+        
+    )
+    readonly_fields=['last_login']
+    add_fieldsets=(
+         (None, {
+            "fields": (
+                'email','password1','password2','name','is_active','is_staff','is_superuser'
+            ),
+        }),
+    )
+    
+
+
+admin.site.register(models.User,UserAdminManager)
+
